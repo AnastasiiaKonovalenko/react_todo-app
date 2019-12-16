835b91d7a0ab0bb0cecc4924d4d5b20bff953a89
natya2203 <nastya_2203@ukr.net>
2019-12-16 11:53:38 +0200

    Updates

---
+++ precache-manifest.8742183f6de9728b783267fb16076c90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e7595a05b343dcc81319dc58f10c5d",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "7b46157b1a9ac190f3fe",
+    "url": "/react_todo-app/static/css/main.abae98c4.chunk.css"
+  },
+  {
+    "revision": "c38d0993d00d5f507090",
+    "url": "/react_todo-app/static/js/2.e6609317.chunk.js"
+  },
+  {
+    "revision": "7b46157b1a9ac190f3fe",
+    "url": "/react_todo-app/static/js/main.39921b1b.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
